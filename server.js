55873fe2fa6c7fb3129f66b7b6e1668b6a625d4c
Manav Mehta <manavmehta@Manavs-MacBook-Air.local>
2025-01-30 18:46:51 +0800

    Database setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize Express App
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Allows JSON requests
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Finwise API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
